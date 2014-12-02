863be809dcb848eb10a95ac5ce8dda978141eb95
capricore <405020841@qq.com>
2014-12-02 12:22:07 +0800

    time:O(n),space:O(1)

---
+++ LinkedList/ReverseKGroup.java
@@ -0,0 +1,58 @@
+package LinkedList;
+
+public class ReverseKGroup
+{
+	public ListNode reverseKGroup(ListNode head, int k)
+	{
+		ListNode dummy = new ListNode(0);
+		dummy.next = head;
+		if (k <= 1)
+		{
+			return head;
+		}
+		ListNode cur = head, pre = dummy;
+		int count = 0;
+		while (cur != null)
+		{
+			count++;
+			if (count % k == 0)
+			{
+				pre = reverse(pre,cur.next);
+				cur = pre;
+			}else {
+				cur = cur.next;
+			}
+		}
+		return dummy.next;
+	}
+	
+	public ListNode reverse(ListNode head, ListNode next)
+	{
+		ListNode last = head.next;
+		ListNode cur = last.next;
+		while(cur != next)
+		{
+			last.next = cur.next;
+			cur.next = head.next;
+			head.next = cur;
+			cur = last.next;
+		}
+		return last.next;
+	}
+	
+	public static void main(String[] args)
+	{
+		ListNode head = new ListNode(1);
+		ListNode cur = head;
+		cur = cur.add(2);
+		cur = cur.add(3);
+		cur = cur.add(4);
+		cur = cur.add(5);
+		head = new ReverseKGroup().reverseKGroup(head, 2);
+		while(head != null)
+		{
+			System.out.println(head.val);
+			head = head.next;
+		}
+	}
+}
