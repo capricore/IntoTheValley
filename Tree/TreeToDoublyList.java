96d2ec68305d01ed2e86549d644e84df74ae0b72
capricore <405020841@qq.com>
2014-12-11 15:59:22 +0800

    test

--- Tree/TreeToDoublyList.java
+++ Tree/TreeToDoublyList.java
@@ -1,5 +1,18 @@
 package Tree;
-
+class TreeNode
+{
+	int val;
+	TreeNode left;
+	TreeNode right;
+	TreeNode(int value)
+	{
+		val = value;
+		left = null;
+		right = null;
+	}
+}
 public class TreeToDoublyList {
-
+	public static void main(String[] args) {
+		
+	}
 }
