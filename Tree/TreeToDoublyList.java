941af7ffd01419beb175779cc84ec14d675f7487
capricore <405020841@qq.com>
2014-12-17 19:01:35 +0800

    time:O(n),space:O(1)

--- Tree/TreeToDoublyList.java
+++ Tree/TreeToDoublyList.java
@@ -12,7 +12,33 @@ class TreeNode
 	}
 }
 public class TreeToDoublyList {
-	public static void main(String[] args) {
-		TreeNode headNode = new TreeNode(0);
+	TreeNode treeToDoublyList(TreeNode root)
+	{
+		TreeNode prev = null;//记录当前指针记录的位置
+		TreeNode head = null;//记录的头指针
+		treeToDoublyList(root, prev, head);
+		return head;
+	}
+	
+	void treeToDoublyList(TreeNode p, TreeNode prev, TreeNode head)
+	{
+		if (p == null) {
+			return;
+		}
+		treeToDoublyList(p.left, prev, head);
+		p.left = prev;
+		if (prev != null) {
+			prev.right = p;
+		}else {
+			head = p;
+		}
+		TreeNode right = p.right;
+		//将首尾相接，简历循环关系
+		head.left = p;
+		p.right = head;
+		//进入下一个循环之前，更新prev节点
+		prev = p;
+		treeToDoublyList(right, prev, head);
+		
 	}
 }
