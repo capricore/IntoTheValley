86003e145ebae0c005cf0aadc43f2d3ac99c8af3
capricore <405020841@qq.com>
2014-12-22 20:06:15 +0800

    time:O(logn),space:O(1)

---
+++ Tree/PathSum.java
@@ -0,0 +1,15 @@
+package Tree;
+
+public class PathSum {
+	public boolean hasPathSum(TreeNode root, int sum)
+	{
+		if (root == null) {
+			return false;
+		}
+		if (root.right == null && root.left == null && root.val == sum) {
+			return true;
+		}else {
+			return hasPathSum(root.left, sum-root.val) || hasPathSum(root.right, sum-root.val);
+		}
+	}
+}
