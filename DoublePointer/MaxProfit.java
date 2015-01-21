a475f8df57d4fd029923f45634070c525bf40ccc
capricore <405020841@qq.com>
2015-01-21 21:55:04 +0800

    time:O(n),space:O(1)

---
+++ DoublePointer/MaxProfit.java
@@ -0,0 +1,17 @@
+package DoublePointer;
+
+public class MaxProfit {
+	public int maxProfit(int prices[]){
+		if (prices.length == 0) {
+			return 0;
+		}
+		int profit = 0;
+		int min = prices[0];//first pointer to record the minimum price
+		for (int i = 1; i < prices.length; i++) {
+			//second pointer to traverse the array
+			min = Math.min(min, prices[i]);
+			profit = Math.max(profit, prices[i] - min);//update the maximum profit
+		}
+		return profit;
+	}
+}
