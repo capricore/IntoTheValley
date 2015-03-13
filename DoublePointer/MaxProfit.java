72edb6e1d2bc5d5a8fcef13d2eeb4d41b4c0c9c9
capricore <405020841@qq.com>
2015-03-13 13:02:20 +0800

    note update

--- DoublePointer/MaxProfit.java
+++ DoublePointer/MaxProfit.java
@@ -6,9 +6,9 @@ public class MaxProfit {
 			return 0;
 		}
 		int profit = 0;
-		int min = prices[0];//first pointer to record the minimum price
+		int min = prices[0];//first pointer records the minimum price
 		for (int i = 1; i < prices.length; i++) {
-			//second pointer to traverse the array
+			//second pointer traverses the array
 			min = Math.min(min, prices[i]);
 			profit = Math.max(profit, prices[i] - min);//update the maximum profit
 		}
