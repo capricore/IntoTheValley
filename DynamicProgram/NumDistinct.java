a555ea00dd3df4e30519c2261d98627d04082de4
capricore <405020841@qq.com>
2015-03-24 14:02:06 +0800

    time:O(n^2),space:O(n^2)

---
+++ DynamicProgram/NumDistinct.java
@@ -0,0 +1,32 @@
+package DynamicProgram;
+
+public class NumDistinct {
+	int numDistinct(String s, String l)
+	{
+		int sLen = s.length();
+		int lLen = l.length();
+		if (sLen == 0 || lLen == 0 || sLen < lLen) {
+			return 0;
+		}
+		int[][] dp = new int[sLen+1][lLen+1];
+		for (int i = 0; i < sLen; i++) {
+			dp[i][0] = 1;//null counts as a sub string.
+		}
+		for (int i = 1; i < sLen; i++) {
+			for (int j = 1; j < lLen; j++) {
+				if (s.charAt(i) == l.charAt(j)) {
+					dp[i][j] = dp[i-1][j] + dp[i][j-1];
+				}else {
+					dp[i][j] = dp[i-1][j];
+				}
+			}
+		}
+		return dp[sLen-1][lLen-1];
+	}
+	
+	public static void main(String[] args) {
+		String s = "aexampplee";
+		String l = "example";
+		System.out.println(new NumDistinct().numDistinct(s, l));
+	}
+}
