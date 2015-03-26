8541cd70f47b6c7802683f3e55e40ef7f3ce6542
capricore <405020841@qq.com>
2015-03-26 14:05:50 +0800

    time:O(nm),space:O(nm)

--- DynamicProgram/EditDistance.java
+++ DynamicProgram/EditDistance.java
@@ -16,8 +16,8 @@ public class EditDistance {
 		for (int i = 0; i < len2; i++) {
 			dp[0][i] = i;
 		}
-		for (int i = 0; i < len1+1; i++) {
-			for (int j = 0; j < len2+1; j++) {
+		for (int i = 1; i < len1+1; i++) {
+			for (int j = 1; j < len2+1; j++) {
 				if (s1.charAt(i-1) == s2.charAt(j-1)) {
 					dp[i][j] = dp[i-1][j-1];
 				}else {
@@ -26,5 +26,11 @@ public class EditDistance {
 				}
 			}
 		}
+		return dp[len1][len2];
+	}
+	
+	public static void main(String[] args) {
+		String s1 = "abc", s2 = "abcd";
+		System.out.println(new EditDistance().editDistance(s1, s2));
 	}
 }
