55ef7512ede038aca27f0899a83e0842791696f0
capricore <405020841@qq.com>
2015-05-27 13:30:52 +0800

    update

--- String/IsMatch.java
+++ String/IsMatch.java
@@ -17,27 +17,24 @@ public class IsMatch {
 		if (j == p.length()) {
 			return i == s.length();
 		}
-		//如果下一个字符不是*的情况
 		if (j == p.length() -1 || p.charAt(j+1) != '*') {
 			if (i == s.length()) {
 				return false;
 			}
 			return (p.charAt(j) == '.' || s.charAt(i) == p.charAt(j)) && isMatch(s, p, ++i, ++j);
 		}
-		while (i < s.length() && (s.charAt(i) == p.charAt(j))) {
-			//跳过*字符，如果匹配到，返回true，否则，继续比较
+		while (i < s.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.')) {
 			if (isMatch(s, p, i, j+2)) {
 				return true;
 			}
 			i++;
 		}
-		//跳过*字符，方便检查s到头的情况
 		return isMatch(s, p, i, j+2);
 	}
 	
 	public static void main(String[] args) {
 		String string = "ab";
-		String p = "a*";
+		String p = "ab.";
 		System.out.println(new IsMatch().isMatch(string, p));
 	}
 }
